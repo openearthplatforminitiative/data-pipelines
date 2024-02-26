e24375ff26d4964f6dfe7d1ca176424a76a5303f
Gil Tinde <giltinde@hotmail.com>
2024-02-26 15:29:41 +0100

    fix CDSClient resource

--- data_pipelines/resources/__init__.py
+++ data_pipelines/resources/__init__.py
@@ -1,4 +1,3 @@
-from data_pipelines.utils.flood.config import GLOFAS_API_URL
 from dagster import EnvVar
 
 from .dask_resource import DaskLocalResource
@@ -27,10 +26,7 @@ RESOURCES = {
     ),
     "zarr_io_manager": ZarrIOManager(base_path=settings.base_data_path),
     "parquet_io_manager": ParquetIOManager(base_path=settings.base_data_path),
-    "client": CDSClient(
-        api_url=GLOFAS_API_URL,
-        api_key=f"{EnvVar('CDS_USER_ID').get_value()}:{EnvVar('CDS_API_KEY').get_value()}",
-    ),
+    "client": CDSClient(user_id=EnvVar("CDS_USER_ID"), api_key=EnvVar("CDS_API_KEY")),
     "grib_io_manager": GribIOManager(),
     "multi_partition_parquet_io_manager": ParquetIOManagerNew(read_all_partitions=True),
     "new_parquet_io_manager": ParquetIOManagerNew(),
--- data_pipelines/resources/glofas_resource.py
+++ data_pipelines/resources/glofas_resource.py
@@ -4,14 +4,22 @@ import cdsapi
 from dagster import ConfigurableResource
 from pydantic import PrivateAttr
 
+from data_pipelines.utils.flood.config import GLOFAS_API_URL
+
 
 class CDSClient(ConfigurableResource):
-    api_url: str
+    api_url: str = GLOFAS_API_URL
+    user_id: str
     api_key: str
     _client: cdsapi.Client = PrivateAttr()
 
+    @property
+    def _user_key(self) -> str:
+        return f"{self.user_id}:{self.api_key}"
+
     def setup_for_execution(self, context) -> None:
-        self._client = cdsapi.Client(url=self.api_url, key=self.api_key)
+        context.log.info(f"Setting up CDS client with user key {self._user_key}")
+        self._client = cdsapi.Client(url=self.api_url, key=self._user_key)
 
     def fetch_data(self, request_params, output_path):
         try:
