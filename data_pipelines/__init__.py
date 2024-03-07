e6759831d2b2b9319b68517c9b780f2b590ceffe
A-Stangeland <aleksander.stangeland@gmail.com>
2024-03-07 09:13:48 +0100

    Add job schedule for flood pipeline

--- data_pipelines/__init__.py
+++ data_pipelines/__init__.py
@@ -1,8 +1,11 @@
 from dagster import Definitions
 
 from .assets import deforestation_assets, flood_assets, river_basin_assets
+from .jobs import glofas_daily_schedule
 from .resources import RESOURCES
 
 all_assets = [*deforestation_assets, *river_basin_assets, *flood_assets]
 
-defs = Definitions(assets=all_assets, resources=RESOURCES)
+defs = Definitions(
+    assets=all_assets, resources=RESOURCES, schedules=[glofas_daily_schedule]
+)
