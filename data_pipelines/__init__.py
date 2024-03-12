a9241b082c18b71ec7643e4239788c970ca8f369
A-Stangeland <aleksander.stangeland@gmail.com>
2024-03-12 13:10:04 +0100

    Move definition of assets to assets/__init__.py

--- data_pipelines/__init__.py
+++ data_pipelines/__init__.py
@@ -1,11 +1,9 @@
 from dagster import Definitions
 
-from .assets import deforestation_assets, flood_assets, river_basin_assets
+from .assets import ALL_ASSETS
 from .jobs import glofas_daily_schedule
 from .resources import RESOURCES
 
-all_assets = [*deforestation_assets, *river_basin_assets, *flood_assets]
-
 defs = Definitions(
-    assets=all_assets, resources=RESOURCES, schedules=[glofas_daily_schedule]
+    assets=ALL_ASSETS, resources=RESOURCES, schedules=[glofas_daily_schedule]
 )
--- data_pipelines/assets/__init__.py
+++ data_pipelines/assets/__init__.py
@@ -3,14 +3,14 @@ from dagster import load_assets_from_package_module
 from . import basins, deforestation, flood
 
 DEFORESTATION = "deforestation"
-BASINS = "basins"
+BASINS = "basin"
 FLOOD = "flood"
 
 deforestation_assets = load_assets_from_package_module(
     package_module=deforestation, group_name=DEFORESTATION
 )
-river_basin_assets = load_assets_from_package_module(
-    package_module=basins, group_name=BASINS
-)
+basin_assets = load_assets_from_package_module(package_module=basins, group_name=BASINS)
 
 flood_assets = load_assets_from_package_module(package_module=flood, group_name=FLOOD)
+
+ALL_ASSETS = [*deforestation_assets, *basin_assets, *flood_assets]
