bd0d663c795edde15f23e57a6194aa1d0e0352ee
A-Stangeland <aleksander.stangeland@gmail.com>
2024-03-12 13:53:04 +0100

    Change basins from multi_asset to single asset

--- data_pipelines/assets/basins/basin.py
+++ data_pipelines/assets/basins/basin.py
@@ -2,38 +2,26 @@ import io
 import zipfile
 
 import httpx
-from dagster import (
-    AssetExecutionContext,
-    AssetKey,
-    AssetOut,
-    MaterializeResult,
-    multi_asset,
-)
+from dagster import AssetExecutionContext, AssetKey, MaterializeResult, asset
 
 from data_pipelines.settings import settings
 
 HYDROSHEDS_URL = (
     "https://data.hydrosheds.org/file/hydrobasins/standard/hybas_af_lev01-12_v1c.zip"
 )
-HYDROSHEDS_BASIN_LEVELS = list(range(1, 13))
+HYDROSHEDS_BASIN_LEVEL = 7
 
 
-@multi_asset(
-    outs={
-        f"basins_level{level:02}": AssetOut(key_prefix="basin")
-        for level in HYDROSHEDS_BASIN_LEVELS
-    }
-)
+@asset(key_prefix="basin")
 def basins(context: AssetExecutionContext):
     r = httpx.get(HYDROSHEDS_URL)
     z = zipfile.ZipFile(io.BytesIO(r.content))
-    for level in HYDROSHEDS_BASIN_LEVELS:
-        file_name = f"hybas_af_lev{level:02}_v1c"
-        asset_name = f"basins_level{level:02}"
-        for extension in ["dbf", "prj", "sbn", "sbx", "shp", "shp.xml", "shx"]:
-            out_path = settings.base_data_upath.joinpath(
-                "basin", "basins", asset_name
-            ).with_suffix(extension)
-            print(out_path)
-            out_path.write_bytes(z.read(file_name))
-        yield MaterializeResult(AssetKey(["basin", asset_name]))
+    file_name = f"hybas_af_lev{HYDROSHEDS_BASIN_LEVEL:02}_v1c"
+    asset_name = "basins"
+    for extension in ["dbf", "prj", "sbn", "sbx", "shp", "shp.xml", "shx"]:
+        out_path = settings.base_data_upath.joinpath(
+            "basin", "basins", asset_name
+        ).with_suffix(extension)
+        print(out_path)
+        out_path.write_bytes(z.read(file_name))
+    return MaterializeResult(AssetKey(["basin", asset_name]))
