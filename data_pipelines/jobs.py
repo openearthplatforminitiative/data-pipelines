e936e5e400600dcfb53c099360f7961e8d3f2c22
Daniel Johnsen <daniel.johnsen@knowit.no>
2024-11-14 14:50:45 +0100

    fix: add automatic retry of failed jobs with linear backoff

--- data_pipelines/jobs.py
+++ data_pipelines/jobs.py
@@ -1,4 +1,15 @@
-from dagster import MAX_RUNTIME_SECONDS_TAG, ScheduleDefinition, define_asset_job
+from dagster import (
+    MAX_RUNTIME_SECONDS_TAG,
+    ScheduleDefinition,
+    RetryPolicy,
+    Backoff,
+    define_asset_job,
+)
+
+# makes job retry with linear backoff. With these settings, the last retry is
+# after 7500 seconds (125 minutes)
+retry_policy = RetryPolicy(max_retries=5, delay=500, backoff=Backoff.LINEAR)
+
 
 # build a job that materializes all flood assets
 # should timeout after 1 hour
@@ -6,6 +17,7 @@ all_flood_assets_job = define_asset_job(
     "all_flood_assets_job",
     selection="flood/raw_discharge*",
     tags={MAX_RUNTIME_SECONDS_TAG: 43000},
+    op_retry_policy=retry_policy,
 )
 
 # define a schedule that runs the all_assets_job every day at 09:30 UTC
